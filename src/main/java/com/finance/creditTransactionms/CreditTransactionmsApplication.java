61e0144b366ca36e734bf1cdb3f64de56172f795
GuadalupeCa <guadalupe.carranzavilla@everis.nttdata.com>
2021-11-11 01:44:19 -0500

    initial push

---
+++ src/main/java/com/finance/creditTransactionms/CreditTransactionmsApplication.java
@@ -0,0 +1,13 @@
+package com.finance.creditTransactionms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditTransactionmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditTransactionmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance/creditTransactionms/CreditTransactionmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.creditTransactionms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditTransactionmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
